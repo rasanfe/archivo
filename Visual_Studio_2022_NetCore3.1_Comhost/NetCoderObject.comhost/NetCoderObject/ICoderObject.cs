15bb705b38e8afb5c47fa619b028f3b67180d0fc
Ramón San Félix Ramón <47455942+rasanfe@users.noreply.github.com>
2022-12-05 12:01:34 +0100

    Add files via upload

---
+++ Visual_Studio_2022_NetCore3.1_Comhost/NetCoderObject.comhost/NetCoderObject/ICoderObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace NetCoderObject
+{
+    [ComVisible(true)]
+    [Guid(ContractGuids.ServerInterface)]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface ICoderObject
+    {
+        public string Base64Encode(Byte[] bytes);
+        public Byte[] Base64Decode(string encoded);
+        public string Base64UrlEncode(Byte[] bytes);
+        public Byte[] Base64UrlDecode(string encodedUrl);
+        public string HexEncode(Byte[] bytes);
+        public Byte[] HexDecode(string hexString);
+        public string Base32Encode(Byte[] bytes);
+        public Byte[] Base32Decode(string encoded);
+
+
+    }
+
+}
