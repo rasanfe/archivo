d224943f6df5e823232b96595eee885a521fc14b
Ramón San Félix Ramón <47455942+rasanfe@users.noreply.github.com>
2022-12-05 11:58:00 +0100

    Add files via upload

---
+++ Visual_Studio_2022_NetCore3.1_Comhost/ExpoCom.comhost/COMServerDemo/COMServer/ContractGuids.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMServer
+{
+    internal sealed class ContractGuids
+    {
+        public const string ServerClass = "1861A57B-ED62-4BD2-8824-D7C18AB5D3CE";
+        public const string ServerInterface = "08058397-C4B6-413A-A70B-0AA8E49E8E81";
+    }
+}
