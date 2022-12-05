44757d159eaf2de0f874ddeac121908cfa96597c
Ramón San Félix Ramón <47455942+rasanfe@users.noreply.github.com>
2022-12-05 11:53:35 +0100

    Add files via upload

---
+++ Visual_Studio_2022_NetCore3.1/ExpoCom_NetCore3.1/COMServerDemo/COMServer/Server.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace COMServer
+{
+    
+    public class Server 
+    {
+        public double ComputePi()
+        {
+            double sum = 0.0;
+            int sign = 1;
+            for (int i = 0; i < 1024; ++i)
+            {
+                sum += sign / (2.0 * i + 1.0);
+                sign *= -1;
+            }
+
+            return 4.0 * sum;
+        }
+    }
+}
