0ea2dbd0e2692f009662559e046382e9effa776a
Ramón San Félix Ramón <47455942+rasanfe@users.noreply.github.com>
2023-11-17 09:58:07 +0100

    Añadimos función CopyDirectory

--- Visual_Studio_2022_NetCore3.1/FileService_NetCore3.1/FileService/FileService.cs
+++ Visual_Studio_2022_NetCore3.1/FileService_NetCore3.1/FileService/FileService.cs
@@ -190,6 +190,69 @@ namespace FileService
 
         }
 
+        public void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+        {
+            string source = @sourceDir;
+            string destination = @destinationDir;
+            try
+            {
+                DirectoryCopy(source, destination, recursive, "*");
+            }
+            catch (Exception ex)
+            {
+                errorText = ex.Message;
+                throw new DirectoryNotFoundException(errorText);
+            }
+
+        }
+
+        public void CopyDirectory(string sourceDir, string destinationDir, bool recursive, string searchPattern)
+        {
+            string source = @sourceDir;
+            string destination = @destinationDir;
+            try
+            {
+                DirectoryCopy(source, destination, recursive, searchPattern);
+            }
+            catch (Exception ex)
+            {
+                errorText = ex.Message;
+                throw new DirectoryNotFoundException(errorText);
+            }
+
+        }
+        static void DirectoryCopy(string sourceDir, string destinationDir, bool recursive, string searchPattern)
+        {
+            // Get information about the source directory
+            var dir = new DirectoryInfo(sourceDir);
+
+            // Check if the source directory exists
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+            // Cache directories before we start copying
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // Create the destination directory
+            Directory.CreateDirectory(destinationDir);
+
+            // Get the files in the source directory and copy to the destination directory
+            foreach (FileInfo file in dir.GetFiles(searchPattern))
+            {
+                string targetFilePath = Path.Combine(destinationDir, file.Name);
+                file.CopyTo(targetFilePath);
+            }
+
+            // If recursive and copying subdirectories, recursively call this method
+            if (recursive)
+            {
+                foreach (DirectoryInfo subDir in dirs)
+                {
+                    string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                    DirectoryCopy(subDir.FullName, newDestinationDir, true, searchPattern);
+                }
+            }
+        }
 
     }
 }
