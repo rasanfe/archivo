9a9b2440f78ca3daa36b3e1cebdb2a251e846a44
Ramón San Félix Ramón <47455942+rasanfe@users.noreply.github.com>
2023-12-13 13:57:16 +0100

    Add files via upload

---
+++ Visual_Studio_2022_NetCore3.1_Comhost/ScannerWia.comhost/ScannerWia/IScannerWia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using WIA;
+
+namespace ScannerWia
+{
+    [ComVisible(true)]
+    [Guid(ContractGuids.ServerInterface)]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IScannerWia
+    {
+        public string[] ListScanners();
+        public string Scan(string scanner, string format, string outputPath, string fileName);
+
+        public void ConvertPDF(string imagePath, string pdfPath);
+
+
+    }
+}
