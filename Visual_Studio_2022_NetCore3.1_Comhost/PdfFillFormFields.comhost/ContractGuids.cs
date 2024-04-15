94dc24c91684ff9436a9990e57099a20f467dfe1
Ramón San Félix Ramón <47455942+rasanfe@users.noreply.github.com>
2024-04-15 08:40:57 +0200

    Add files via upload

---
+++ Visual_Studio_2022_NetCore3.1_Comhost/PdfFillFormFields.comhost/ContractGuids.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PdfFillFormFields
+{
+
+        internal sealed class ContractGuids
+        {
+            public const string ServerClass = "ECD742BC-1795-43CF-A488-C8ABD046CAAA";
+            public const string ServerInterface = "90E6A1D1-DF85-4716-8B74-939E4647A9E3";
+        }
+
+
+}
---
+++ Visual_Studio_2022_NetCore3.1_Comhost/PdfFillFormFields.comhost/IPdfFill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PdfFillFormFields
+{
+    [ComVisible(true)]
+    [Guid(ContractGuids.ServerInterface)]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IPdfFill
+    {
+        public string FillFormFields(string inputFile, string outputFile, string[] fieldNames, string[] dataField);
+        public string GetFormFields(string inputFile, ref string[] fieldNames);
+       
+
+    }
+}
